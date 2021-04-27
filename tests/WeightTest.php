4793ebc7b1f425045f31fa5b820c5afaf3e0352e
ket sakda <ketsakda@kets-MacBook-Air.local>
2021-04-27 13:21:14 +0700

    finish WeightConversion

---
+++ tests/WeightTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Kravanh\UnitConversions\Tests;
+
+use Kravanh\UnitConversions\Weight;
+use PHPUnit\Framework\TestCase;
+
+class WeightTest extends TestCase
+{
+    public function test_it_can_convert_kilograms_to_lbs()
+    {
+        $lbs = Weight::fromKilograms(100)->toLbs();
+        $this->assertEquals(220.4623, $lbs);
+    }
+}
